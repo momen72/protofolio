65dec00353e8af50201164053aea8921fb6fc45f
momen72 <66283391+momen72@users.noreply.github.com>
2026-05-02 17:27:56 +0300

    sssa

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
                 </p>
 
                 <div className='flex gap-3 mt-8'>
-                    <button className="bg-[#A6FF5D] hover:bg-[#A6FF5D]/90 text-gray-800 px-6 py-2.5 rounded-full text-sm transition cursor-pointer group" onClick={() => navigate("/aboute")}>
+                    <button className="bg-[#A6FF5D] hover:bg-[#A6FF5D]/90 text-gray-800 px-6 py-2.5 rounded-full text-sm transition cursor-pointer group" onClick={() => sectionRef.current.scrollIntoView()}>
                          <div className="relative overflow-hidden">
                             <span className="block transition-transform duration-200 group-hover:-translate-y-full">
                                 Get Started today
@@ -61,7 +61,10 @@ const Home = () => {
                     <p className='text-sm text-white/50'>Scroll down</p>
                 </div>
             </header>
+            <div ref={sectionRef}>
+
             <About/>
+            </div>
             <div ref={sectionRef}>
             
             <Products/>
